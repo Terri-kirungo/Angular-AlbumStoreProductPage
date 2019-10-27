afe9ce1a4a72b0976e6cd921a2de9bb600311158
Terri <terrykirungo@gmail.com>
2019-10-27 18:41:35 +0300

    Added a type to getAlbum, imported the Album interface and added a type to the albumInfo property

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -6,14 +6,13 @@ import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
-
   
   private _albumUrl = '../assets/album.json';
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map((response) => response.json());    
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json());    
   }
 
 }
